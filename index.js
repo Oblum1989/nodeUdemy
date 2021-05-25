2e9665f80708c2b9a34b5f0046e22ada990d931b
oscar blum <oscar.blum@gmail.com>
2021-05-24 23:07:31 -0500

    modulate server

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const http = require('http');
-const url = require('url');
-const { StringDecoder } = require('string_decoder');
-const router = require("./router")
+const requestHandler = require("./request_handler")
 
 global.resources = {
   mascotas: [
@@ -13,65 +11,7 @@ global.resources = {
   ]
 }
 
-const server = http.createServer((req, res) => {
-  const currentUrl = req.url
-  const parseUrl = url.parse(currentUrl, true)
-
-  const path = parseUrl.pathname
-
-  const pathClean = path.replace(/^\/+|\/+$/g, '')
-
-  const method = req.method.toLowerCase()
-
-  const {query={}} = parseUrl
-
-  const {headers={}} = req
-
-  const decoder = new StringDecoder('utf8');
-  let buffer = ''
-  req.on('data', (data)=>{
-    buffer += decoder.write(data)
-  })
-  req.on('end', ()=>{
-    buffer += decoder.end()
-
-    if (headers["content-type"] === "application/json") {
-      buffer = JSON.parse(buffer)
-    }
-
-    if(pathClean.indexOf("/") > -1){
-      var [principalPath, indice] = pathClean.split('/')
-    }
-
-    const data = {
-      indice,
-      path: principalPath || pathClean,
-      query,
-      method,
-      headers,
-      payload: buffer
-    }
-
-    console.log({data})
-
-    let handler
-    if (data.path && router[data.path] && router[data.path][method]) {
-      handler = router[data.path][method]
-    }else{
-      handler = router.noFound
-    }
-
-    if (typeof handler === 'function') {
-      handler(data, (statusCode = 200, message)=>{
-        const response = JSON.stringify(message)
-        res.setHeader("Content-Type", "application/json")
-        res.writeHead(statusCode)
-        res.end(response)
-      })
-    }
-  })
-
-});
+const server = http.createServer(requestHandler)
 
 server.listen(5000, ()=>{
   console.log('el servidor ha sido montado')
